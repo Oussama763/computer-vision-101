18e85b44f4e6342dde5c6bab205b095cbeca7145
oussama <sarbouteoussama@gmail.com>
2026-08-23 20:04:45 +0100

    added detect_objects

--- img_ops/basic_ops.py
+++ img_ops/basic_ops.py
@@ -117,8 +117,8 @@ def blur_image(image: str) -> None:
 
     gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    normal_blur = cv.blur(img, (5, 5))
-    gauss_blur = cv.GaussianBlur(img, (5, 5), sigmaX=0)
+    normal_blur = cv.blur(gray_img, (5, 5))
+    gauss_blur = cv.GaussianBlur(gray_img, (5, 5), sigmaX=0)
 
     cv.imshow("original", img)
     cv.imshow("normal", normal_blur)
@@ -146,21 +146,72 @@ def edges_image(image: str) -> None:
 
     gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    """normal_blur = cv.blur(img, (5, 5))
-    gauss_blur = cv.GaussianBlur(img, (5, 5), sigmaX=0)"""
+    normal_blur = cv.blur(gray_img, (5, 5))
+    gauss_blur = cv.GaussianBlur(gray_img, (5, 5), sigmaX=0)
 
-    edges = cv.Canny(img, threshold1=50, threshold2=150)
+    edges_original = cv.Canny(img, threshold1=50, threshold2=150)
+    edges_normal = cv.Canny(normal_blur, threshold1=50, threshold2=150)
+    edges_gaussian = cv.Canny(gauss_blur, threshold1=50, threshold2=150)
 
+    kernel = np.ones((3, 3))
+    dilated_gaussian = cv.dilate(edges_gaussian, kernel, iterations=1)
 
-
-    cv.imshow("edges", edges)
+    #cv.imshow("edges_original", edges_original)
+    #cv.imshow("edges_normal", edges_normal)
+    cv.imshow("dilated_gaussian", dilated_gaussian)
     #cv.imshow("normal", normal_blur)
     #cv.imshow("gaussian", gauss_blur)
 
     cv.waitKey(0)
     cv.destroyAllWindows()
 
-#lower_red = np.array([0, 50, 50])  
+
+
+def detect_objects(image: str, min_area: int) -> None:
+    """Shows the objects in the image
+        
+    Args:
+        image (str): name of the image that should be in the directory img_ops/images/    ----    (Example: image_name.png)
+        min_area (int): minimum area of the object to be detected
+
+    Returns:
+        None
+    """
+    img = cv.imread("images/"+image)
+    if img is None:
+        print("The image you entered is not found, please read the documentation of resize_image()")
+        return
+
+    output_img = img.copy()
+
+    gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    blur_img = cv.GaussianBlur(gray_img, (5, 5), sigmaX=0)
+    edges_img = cv.Canny(blur_img, threshold1=50, threshold2=150)
+    kernel = np.ones((3, 3))
+    dilated_img = cv.dilate(edges_img, kernel, iterations=1)
+
+    contours, hierarchy = cv.findContours(dilated_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    count = 0
+
+    for contour in contours:
+        area = cv.contourArea(contour)
+
+        if area >= min_area:
+            count += 1
+            x, y, w, h = cv.boundingRect(contour)      #draw rectangle around object
+            cv.rectangle(output_img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+            cv.putText(output_img, "text", (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+    print(f"{count} objects")
+
+    cv.imshow("dialated", dilated_img)
+    cv.imshow("objects", output_img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+#lower_red = np.array([0, 50, 50])
 #upper_red = np.array([10, 255, 255])
 
-edges_image("painting.png")
+
+detect_objects("painting.png", 500)
